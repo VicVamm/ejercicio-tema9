c05a0cbadf084ce67c7ce36037b720ede85c47bb
VicVamm <119972463+VicVamm@users.noreply.github.com>
2023-01-12 13:18:27 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        
+
         Cliente cliente1 = new Cliente();
+
 
         cliente1.setEdad(25);
         cliente1.setNombre("Victor");
         cliente1.setTelefono(755859424);
+        cliente1.setCredito(596699.00);
 
-        System.out.println("Edad: " + cliente1.getEdad());
-        System.out.println("Nombre del Cliente: " + cliente1.getNombre());
-        System.out.println("Telefono del cliente: " + cliente1.getTelefono());
+        System.out.println("soy el cliente: " + cliente1.getNombre() + ", mi edad es de " + cliente1.getEdad() + ", mi numero de telefono es: " + cliente1.getTelefono() + " y tengo un crédito de: $" + cliente1.getCredito());
+
+        Trabajador trabajador = new Trabajador();
+
+        trabajador.setEdad(40);
+        trabajador.setNombre("Alfredo");
+        trabajador.setTelefono(94944040);
+        trabajador.setSalario(400);
+
+        System.out.println("soy el trabajador: " + trabajador.getNombre() + ", con " + trabajador.getEdad() + " años, mi numero de telefono es: " + trabajador.getTelefono() + "y mi salario es de: $" + trabajador.getSalario() );
+
+
+
     }
 }
 class Persona {
